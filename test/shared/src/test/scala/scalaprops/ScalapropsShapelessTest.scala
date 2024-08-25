d60dac02c0f9e3c9438adc8584ebfccf69e35622
xuwei-k <6b656e6a69@gmail.com>
2024-08-26 06:26:39 +0900

    enable scala-native test

--- test/shared/src/test/scala/scalaprops/ScalapropsShapelessTest.scala
+++ test/shared/src/test/scala/scalaprops/ScalapropsShapelessTest.scala
@@ -2,13 +2,14 @@ package scalaprops
 
 import scalaprops.ScalapropsShapeless.*
 
-object ScalapropsShapelessTest extends Scalaprops {
-  sealed trait A1 extends Product
-  object A1 {
-    final case class A2(x: Int) extends A1
-    final case class A3(x1: Boolean, x2: Long) extends A1
-  }
+sealed trait A1 extends Product
 
+object A1 {
+  final case class A2(x: Int) extends A1
+  final case class A3(x1: Boolean, x2: Long) extends A1
+}
+
+object ScalapropsShapelessTest extends Scalaprops {
   val genTest1 = Property.forAll { (seed: Long) =>
     Gen[A1].infiniteIterator(seed = seed).take(100).toList.groupBy(_.getClass.getName).size == 2
   }
